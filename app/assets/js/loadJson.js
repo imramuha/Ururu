23a2f4027492375d8c97f4fa8eedd2290886b29f
unknown <im_1996@yahoo.de>
2024-01-12 02:01:34 +0100

    preparing to deploy on github pages

--- app/assets/js/loadJson.js
+++ app/assets/js/loadJson.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import  projects  from '../data/projects.json';
-import  courses  from '../data/aboutus.json';
-import  posts  from '../data/blogposts.json';
-import  articles  from '../data/articles.json';
+import { projects } from '../data/projects.json';
+import { courses } from '../data/aboutUs.json';
+import { posts } from '../data/blogposts.json';
+import { articles } from '../data/articles.json';
 
 export default class LoadJson {
     constructor() {
